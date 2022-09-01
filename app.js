630dd7fef2c738739b027c75d4b9119f7aa204dd
nemanjastojanovich <109088925+nemanjastojanovich@users.noreply.github.com>
2022-09-01 15:57:12 +0200

    second commit

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.post("/failure", function(req, res) {
 
 
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server is running on port 3000.");
 });
 
@@ -75,3 +75,5 @@ app.listen(3000, function() {
 
 // audience ID
 // 3f7c494319
+//
+// ovo radi
